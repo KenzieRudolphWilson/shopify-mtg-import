ff58343bd847ec6ba29dc9c2b16dfcd7fbbcbae0
Taras Gordiyenko <taras.gordiyenko@gmail.com>
2014-06-26 08:51:57 -0400

    Finished buildCardSet.py and added better cropping to main script

--- buildCardSet.py
+++ buildCardSet.py
@@ -3,16 +3,38 @@ import json
 import sys
 import codecs
 
+
+#simple script to parse the mtgJSON data and build a JSON array of unique card names for other code to consume
+#run once, then forget about it until there are new sets in the MTGJSON
+
+def loadCardJSON():
+	jsonData = open('AllSetsArray.json')
+	data = json.load(jsonData)
+	jsonData.close()
+	return data
+
+
+#python witchery to get stdout to print UTF-8
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout) 
-jsonData = open('AllSetsArray.json')
-data = json.load(jsonData)
-jsonData.close()
-i = 0
-for set in data:
-  for cards in set.get("cards"):
-    i += 1
+
+data = loadCardJSON()
+
+cardCounter = 0
+nameSet = set()
+for cardSet in data:
+  for cards in cardSet.get("cards"):
+    cardCounter += 1
     cardname = unicode(cards.get("name"))
     cardname.encode('utf-8')
-    print cardname
+    nameSet.add(cardname)
 
-print 'hello' + str(i)
+
+#print str(nameSet)
+namesFile = codecs.open("cardNameSet.json", "w", "utf-8")
+
+cardsJSON =  json.dumps(sorted(list(nameSet)), sort_keys=True,indent=4, separators=(',', ': '))
+namesFile.write(cardsJSON)
+namesFile.close()
+
+#print 'cardinality of generated set: ' + str(len(nameSet)) 
+#print 'total cardsnames found in JSON: ' + str(cardCounter)
