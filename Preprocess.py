fb55c2d3c49d93ee8b9f00de49806f1590a0cc65
Taras Gordiyenko <taras.gordiyenko@gmail.com>
2014-06-26 23:32:01 -0400

    better image subsection selection, added fuzzy searching for most similar card names

--- Preprocess.py
+++ Preprocess.py
@@ -4,18 +4,59 @@ import numpy as np
 import cv2
 import OCR
 import json
+import buildCardSet as bc
+import difflib as dl
 
-def checkForCard(text):
-    jsonData = open('AllSetsArray.json')
-    data = json.load(jsonData)
-    jsonData.close()
+#rectangular crop
+def crop(image, x, y, length, height):
+    return image[(x):(x+length), (y):(y+height)]
 
+#removes pixels from each edge
+def chopOffEdges(image, left, right, top, bottom):
+    return image[(left):(len(image[0]) - right), (top):(len(image[1]) - bottom)]
+
+def getNameList():
+    try:
+        jsonData = open('cardNameSet.json')
+        data = json.load(jsonData)
+        jsonData.close()
+    except IOError:
+        jsonData = bc.generateCardMap
+        bc.saveUTF8File(jsonData, 'cardNameSet.json')
+        data = json.loads(jsonData)
+    return list(data)
+
+
+def findMostSimilar(imageText, vocabulary):
+    bestSimilarity = 0
+    mostSimilarWord = ''
+    for word in vocabulary:
+        seq = dl.SequenceMatcher(None, imageText, word)
+        similarity = seq.ratio()
+        if similarity > bestSimilarity:
+            bestSimilarity = similarity
+            mostSimilarWord = word
+    return mostSimilarWord
+
+
+def drawCaptureBox(image, x, y, length, height):
+    cv2.line(image,(x, y),(x ,y + height),(0,255,0),2)
+    cv2.line(image,(x ,y + height),(x + length, y + height),(0,255,0),2)
+    cv2.line(image,(x + length, y + height),(x + length, y),(0,255,0),2)
+    cv2.line(image,(x + length, y),(x, y),(0,255,0),2)
+    return image
+
+
+def checkForCard(text, names):
+    return findMostSimilar(text, names)
     
+
+
+nameList = getNameList()
 
 cap = cv2.VideoCapture(0)
 t = 20
 thresh =1
-canny = 1
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -26,37 +67,21 @@ while(True):
     except:
     	pass
 
-    minLineLength = 200
-    maxLineGap = 20
-    x1 = 0
-    y1 = 0
-    x2 = 380
-    y2 = 0
-    x3 = 380
-    y3 = 50
-    x4 = 0
-    y4 = 50
-    x = 63
-    y = 60
-    cv2.line(frame,(x + x1,y + y1),(x + x2,y + y2),(0,255,0),2)
-    cv2.line(frame,(x + x2,y + y2),(x + x3,y + y3),(0,255,0),2)
-    cv2.line(frame,(x + x3,y + y3),(x + x4,y + y4),(0,255,0),2)
-    cv2.line(frame,(x + x4,y + y4),(x + x1,y + y1),(0,255,0),2)
-    #lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength, maxLineGap)
-    #if lines is not None:
-    #	for x1, y1, x2, y2 in lines[0]:
-    #		cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),2)
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
+    
     #cv2.imshow('edges',edges)
-    cropped = frame[(y+y1):(y+y3), (x+x1):(x+x2)]
+    x = 100
+    y = 100
+    length = 200
+    height = 50
+    cropped = crop(frame, x, y, length, height)
+    frame = drawCaptureBox(frame, x, y, length, height)
 	
     if(thresh > 0):
         cropped = cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY)    
     	cropped = cv2.adaptiveThreshold(cropped, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,15, t)
 
-    cropped = cropped[(2):(y3-2), (2):(x2-2)]
-
+    cropped = chopOffEdges(cropped, 2, 2, 2, 2)
+    cv2.imshow('frame',frame)
     cv2.imshow('cropped',cropped)
 
 
@@ -71,7 +96,9 @@ while(True):
     	thresh = -thresh
     if key & 0xFF == ord('p'):
         cv2.imwrite('capture.png', cropped)
-        checkForCard(OCR.giveMeText())
+        text = OCR.giveMeText()
+        print "Read Text :" + text
+        print "Most Similar :" + checkForCard(text, nameList)
 
 
 # When everything done, release the capture
--- buildCardSet.py
+++ buildCardSet.py
@@ -4,8 +4,7 @@ import sys
 import codecs
 
 
-#simple script to parse the mtgJSON data and build a JSON array of unique card names for other code to consume
-#run once, then forget about it until there are new sets in the MTGJSON
+
 
 def loadCardJSON():
 	jsonData = open('AllSetsArray.json')
@@ -14,27 +13,35 @@ def loadCardJSON():
 	return data
 
 
-#python witchery to get stdout to print UTF-8
-sys.stdout = codecs.getwriter('utf-8')(sys.stdout) 
+#simple function to parse the mtgJSON data and build a JSON array of unique card names for other code to consume
+#returns S
 
-data = loadCardJSON()
+def generateCardMap():
+	#python witchery to get stdout to print UTF-8
+	#sys.stdout = codecs.getwriter('utf-8')(sys.stdout) 
+	
+	data = loadCardJSON()
+	
+	#cardCounter = 0
+	nameSet = set()
+	for cardSet in data:
+	  for cards in cardSet.get("cards"):
+	    #cardCounter += 1
+	    cardname = unicode(cards.get("name"))
+	    cardname.encode('utf-8')
+	    nameSet.add(cardname)
+	
+	
+	#print str(nameSet)
+	
+	cardsJSON =  json.dumps(sorted(list(nameSet)), sort_keys=True,indent=4, separators=(',', ': '))
+	#print 'cardinality of generated set: ' + str(len(nameSet)) 
+	#print 'total cardsnames found in JSON: ' + str(cardCounter)
 
-cardCounter = 0
-nameSet = set()
-for cardSet in data:
-  for cards in cardSet.get("cards"):
-    cardCounter += 1
-    cardname = unicode(cards.get("name"))
-    cardname.encode('utf-8')
-    nameSet.add(cardname)
+	return cardsJSON
 
 
-#print str(nameSet)
-namesFile = codecs.open("cardNameSet.json", "w", "utf-8")
-
-cardsJSON =  json.dumps(sorted(list(nameSet)), sort_keys=True,indent=4, separators=(',', ': '))
-namesFile.write(cardsJSON)
-namesFile.close()
-
-#print 'cardinality of generated set: ' + str(len(nameSet)) 
-#print 'total cardsnames found in JSON: ' + str(cardCounter)
+def saveUTF8File(content, name):
+	namesFile = codecs.open(name, "w", "utf-8")
+	namesFile.write(content)
+	namesFile.close()
